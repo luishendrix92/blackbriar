4ac4d9be1a9f0cde9862343724bc9632590dc5b6
Luis Lopez <luishendrix92@gmail.com>
2019-06-25 20:19:48 -0700

    cleaned inbox controller

--- src/main/java/com/bootcamp/blackbriar/controller/InboxController.java
+++ src/main/java/com/bootcamp/blackbriar/controller/InboxController.java
@@ -1,10 +1,5 @@
 package com.bootcamp.blackbriar.controller;
 
-import java.security.Principal;
-
-import com.bootcamp.blackbriar.model.inbox.MessageEntity;
-import com.bootcamp.blackbriar.model.inbox.MessageResponse;
-import com.bootcamp.blackbriar.model.inbox.SocketMessage;
 import com.bootcamp.blackbriar.service.inbox.InboxService;
 
 import org.modelmapper.ModelMapper;
