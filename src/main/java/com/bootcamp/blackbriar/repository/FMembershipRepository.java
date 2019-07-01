6935a3a1be82711363b20e980753a9815e248076
Luis Lopez <luishendrix92@gmail.com>
2019-06-30 22:08:09 -0700

    added end forum algorithm and controller endpoint

--- src/main/java/com/bootcamp/blackbriar/repository/FMembershipRepository.java
+++ src/main/java/com/bootcamp/blackbriar/repository/FMembershipRepository.java
@@ -5,12 +5,18 @@ import java.util.Optional;
 
 import com.bootcamp.blackbriar.model.forum.FMembershipEntity;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FMembershipRepository extends CrudRepository<FMembershipEntity, Long> {
   Optional<FMembershipEntity> findByMemberStudentUserId(String userId);
+
   List<FMembershipEntity> findByForumId(long forumId);
+
   Optional<FMembershipEntity> findByMemberStudentUserIdAndForumId(String userId, long forumId);
+
+  @Query("SELECT COUNT(*) FROM ForumMembership M LEFT JOIN M.feedback F WHERE M.forum.id = ?1 AND M.warlock = true AND F IS NULL")
+  Long warlockAmount(long forumId);
 }
